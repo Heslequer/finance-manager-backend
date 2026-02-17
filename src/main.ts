8c6be5d3b6f5f91abcb11a0d91d76424a6fa5d30
Heslequer <hesleiquer5@gmail.com>
2026-02-17 20:29:59 -0300

    feat: update cors config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173, https://finance-manager-frontend-mu.vercel.app',
     credentials: true,
   });
   app.useGlobalPipes(
